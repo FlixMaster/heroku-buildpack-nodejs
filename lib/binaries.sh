c205d997b4b66197f03cd38b698d2e5c97565791
Justin Trautman <justin_trautman@hotmail.com>
2016-12-05 10:57:11 -0700

    Add support for specifying a yarn version

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -8,10 +8,17 @@ needs_resolution() {
 }
 
 install_yarn() {
-  local dir="$1"
+  local version="$1"
+  local dir="$2"
+
+  if needs_resolution "$version"; then
+    echo "Yarn version number does not match MAJOR.MINOR.PATCH format, using latest version"
+    local download_url="https://yarnpkg.com/latest.tar.gz"
+  else
+    local download_url="https://yarnpkg.com/downloads/$version/yarn-v$version.tar.gz"
+  fi
 
   echo "Downloading and installing yarn..."
-  local download_url="https://yarnpkg.com/latest.tar.gz"
   local code=$(curl "$download_url" -L --silent --fail --retry 5 --retry-max-time 15 -o /tmp/yarn.tar.gz --write-out "%{http_code}")
   if [ "$code" != "200" ]; then
     echo "Unable to download yarn: $code" && false
